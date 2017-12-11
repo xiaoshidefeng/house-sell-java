2281b2bf3615eb6a1d6cb003b192f736d11078fd
xiaoshidefeng <1330661071@qq.com>
2017-12-11 13:43:43 +0800

    Initial commit

---
+++ src/main/java/top/chenwe/houseselljava/HouseSellJavaApplication.java
@@ -0,0 +1,12 @@
+package top.chenwe.houseselljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseSellJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseSellJavaApplication.class, args);
+	}
+}
